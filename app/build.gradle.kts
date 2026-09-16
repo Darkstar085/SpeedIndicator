abe8163fd6bed50705627245d83749de1416831d
S I P U N <sipunkumar85@gmail.com>
2026-09-16 22:23:47 +0530

    build: bump app version to 2.0
    - Set the application version code and version name to 2.0.
    
    Change-Id: I2f7c9a4e1b6d8c3f5a0e7b9d2c4f6a8e1b3d5c70
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.sipun.netspeedindicator"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
